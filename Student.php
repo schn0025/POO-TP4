0d7ba69d59a570be2b9b31b6ace7a5b7cfea740a
Arthur Schneider <arthur.schneider@etudiant.univ-reims.fr>
2023-03-01 16:56:32 +0100

    ajout de la suite du TP4 jusqu'a question 19

--- Student.php
+++ Student.php
@@ -63,7 +63,7 @@ class Student{
      * $index ou renvoi une erreur ci l'a valeur de $index n'est pas dans les indice de
      * marks
      *
-     * @throws renvoi une value erreur ci index est trop grand ou negatif
+     * @throws OutOfRangeException renvoi une value erreur ci index est trop grand ou negatif
      * @param integer $index indice de la valeur chercher
      */
     public function getMark(int $index): float // throw OutOfRangeException
@@ -73,6 +73,12 @@ class Student{
         }
         return $this->marks[$index];
     }
+    /**
+     * setMark modifi un note a un indice donné
+     *
+     * @param integer $index indice de la note
+     * @param float $note nouvelle note
+     */
     public function setMark(int $index, float $note)
     {
         if($index < 0 || $index >= count($this->marks)){
@@ -101,4 +107,171 @@ class Student{
         }
         return $rep;
     }
+    /**
+     * getMarksCount retourne le nombre de notes dont dispose un etudiant
+     *
+     * @return integer nombre de note de l'etudiant
+     */
+    public function getMarksCount(): int
+    {
+        $nbNote = 0;
+        foreach($this->marks as $note){
+            $nbNote++;
+        }
+        return $nbNote;
+    }
+    /**
+     * getMean retourne la moyenne des notes d'un etudiant
+     *
+     * @throws DivisionByZeroError renvoi un erreur si il n'y a pas de note
+     * @return float renvoi la moyenne des notes
+     */
+    public function getMean():float
+    {
+        if($this->getMarksCount() == 0){
+            throw new DivisionByZeroError ( "getMark - Division par 0");
+        }
+        $somme = 0;
+        foreach($this->marks as $note){
+            $somme + $note;
+        }
+        return $somme / $this->getMarksCount();
+    }
+    /**
+     * getMinimum retourne la note minimale obtenue par un  etudiant
+     *
+     * @return float renvoi la plus petit note de l'etudiant
+     */
+    public function getMinimum():float
+    {
+        $mini = $this->getMark(0);
+        foreach($this->marks as $note){
+            if($mini == -1){
+                $mini = $note;
+            }
+            elseif($note < $mini){
+                $mini = $note;
+            }
+        }
+        return $mini;
+    }
+    /**
+     * getMaximumIndex retourne l’indice de la note maximale obtenue par un etudiant
+     *
+     * @return integer
+     */
+    public function getMaximumIndex():int
+    {
+        $this->getMark(0);
+        $maxI = 0;
+        foreach($this->marks as $id=>$note){
+            if($this->marks[$maxI] < $note){
+                $maxI = $id;
+            }
+        }
+        return $maxI;
+    }
+    /**
+     * contains prend en paraetre une note. Elle retourne true si 
+     * la note est presente parmi les notes de l’etudiant, false sinon
+     *
+     * @param float $note note rechercher
+     * @return boolean True ci l'etudiant a la note et False sinon
+     */
+    public function contains(float $note): bool
+    {
+        $i = 0;
+        $rep = False;
+        while($i < $this->getMarksCount() && !$rep){
+            if(abs($this->marks[$i] - $note) < 0.0001){
+                $rep = True;
+            }
+            $i++;
+        }
+        return $rep;
+    }
+    /**
+     * getOccurenceCount prend en parametre une note. Elle retourne le nombre de fois 
+     * ou la note est presente parmi les notes de l’etudian
+     *
+     * @param float $note note a chercher
+     * @return integer nombre de fois ou la note apparet
+     */
+    public function getOccurenceCount(float $note): int
+    {
+        $nb = 0;
+        foreach($this->marks as $noteIn){
+            if(abs($noteIn - $note) < 0.0001){
+                $nb ++;
+            }
+        }
+        return $nb;
+    }
+    /**
+     * getFirstOccurrenceIndex prend en parametre une note. Elle retourne 
+     * l’indice de la premiere occurrence de la note parmi les notes de l’etudiant
+     *
+     * @throws UnexpectedValueException renvoi une erreu ci la valeur n'est pas presente
+     * @param float $note note a chercher
+     * @return integer indice de la premiere occurrence
+     */
+    public function getFirstOccurrenceIndex(float $note):int
+    {
+        if(!$this->contains($note)){
+            throw new UnexpectedValueException ("note non presente");
+        }
+        $i = 0;
+        $trouver = False;
+        while($i < $this->getMarksCount() && !$trouver){
+            if(abs($this->marks[$i] - $note) < 0.0001){
+                $trouver = True;
+            }
+            else{
+                $i++;
+            }
+        }
+        return $i;
+    }
+    /**
+     * getLastOccurrenceIndex prend en parametre une note. Elle retourne l’indice de 
+     * la derniere occurrence de la note parmi les notes de l’ ́etudian
+     *
+     * @throws UnexpectedValueException renvoi une erreu ci la valeur n'est pas presente
+     * @param float $note note a chercher
+     * @return integer indice de la derrnier occurrence 
+     */
+    public function getLastOccurrenceIndex(float $note): int
+    {
+        if(!$this->contains($note)){
+            throw new UnexpectedValueException ("note non presente");
+        }
+        $i = 0;
+        $trouver = 0;
+        $occ = $this->getOccurenceCount($note);
+        while($i < $this->getMarksCount() && $trouver < $occ){
+            if(abs($this->marks[$i] - $note) < 0.0001){
+                $trouver++ ;
+            }
+            else{
+                $i++;
+            }
+        }
+        return $i;
+    }
+    /**
+     * swapMarks rend en parametre deux indices. Elle  ́echange le contenu de 
+     * deux cases de l’attribut d’instance marks
+     * dont les indices sont pass ́es en parametre
+     *
+     * @param integer $ind1 indice de la 1er note 
+     * @param integer $ind2 indice de la 2eme note
+     */
+    public function swapMarks(int $ind1, int $ind2)
+    {
+        $note1 = $this->getMark($ind1);
+        $note2 = $this->getMark($ind2);
+        $this->setMark($ind1, $note2);
+        $this->setMark($ind2, $note1);
+    }
+    
 }
--- TestStudent.php
+++ TestStudent.php
@@ -62,10 +62,107 @@ catch (OutOfRangeException $e)
 //Quetion 9
 echo "\n Question 9 \n";
 echo "$kevin \n";
-$kevin->setMark(1,0);
+$kevin->setMark(1,5);
 echo "$kevin \n";
 
 //Quetion 10
 echo "\n Question 10 \n";
 echo decodeBooleen($kevin->isEqual($kevin)),"\n";
 echo decodeBooleen($louis->isEqual($kevin)),"\n";
+
+// Question 11
+echo "\n Question 11 \n";
+echo $kevin->getMarksCount(),"\n";
+echo $louis->getMarksCount(),"\n";
+
+//Quetion 12
+echo "\n Question 12 \n";
+try
+{
+    echo $kevin->getMean(),"\n";
+    echo $louis->getMean(),"\n";
+}
+catch (DivisionByZeroError $e)
+{
+    //gestion de l’exception, par exemple :
+    echo $e->getMessage()."\n";
+}
+
+//Quetion 13
+echo "\n Question 13 \n";
+try
+{
+    echo $kevin->getMinimum(),"\n";
+    echo $louis->getMinimum(),"\n";
+}
+catch (OutOfRangeException $e)
+{
+    //gestion de l’exception, par exemple :
+    echo $e->getMessage()."\n";
+}
+
+//Quetion 14
+echo "\n Question 14 \n";
+echo $kevin,"\n";
+try
+{
+    echo $kevin->getMaximumIndex(),"\n";
+    echo $louis->getMaximumIndex(),"\n";
+}
+catch (OutOfRangeException $e)
+{
+    //gestion de l’exception, par exemple :
+    echo $e->getMessage()."\n";
+}
+
+//Quetion 15
+echo "\n Question 15 \n";
+echo decodeBooleen($kevin->contains(5)),"\n";
+echo decodeBooleen($louis->contains(20)),"\n";
+
+//Quetion 16
+echo "\n Question 16 \n";
+echo $kevin->getOccurenceCount(5),"\n";
+echo $louis->getOccurenceCount(5),"\n";
+
+//Quetion 17
+echo "\n Question 17 \n";
+try
+{
+    echo $kevin->getFirstOccurrenceIndex(5),"\n";
+    echo $louis->getFirstOccurrenceIndex(5),"\n";
+}
+catch (UnexpectedValueException $e)
+{
+    //gestion de l’exception, par exemple :
+    echo $e->getMessage()."\n";
+}
+
+//Quetion 18
+echo "\n Question 18 \n";
+echo $kevin,"\n";
+try
+{
+    echo $kevin->getLastOccurrenceIndex(16.25),"\n";
+    echo $louis->getLastOccurrenceIndex(5),"\n";
+}
+catch (UnexpectedValueException $e)
+{
+    //gestion de l’exception, par exemple :
+    echo $e->getMessage()."\n";
+}
+
+//Quetion 19
+echo "\n Question 19 \n";
+echo $kevin,"\n";
+try
+{
+    echo $kevin->swapMarks(0,2),"\n";
+    echo $louis->swapMarks(0,3),"\n";
+}
+catch (OutOfRangeException $e)
+{
+    //gestion de l’exception, par exemple :
+    echo $e->getMessage()."\n";
+}
+echo $kevin;
